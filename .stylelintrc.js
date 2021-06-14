2bd3cf1a57081787beff9d9e199dddcfd628abde
wenlin <569958268@qq.com>
2021-06-14 23:12:02 +0800

    feat: style

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-  extends: ["stylelint-config-standard-scss", "stylelint-config-prettier"],
-  plugins:["stylelint-order"],
-  // rules: { 
-  //   "declaration-colon-space-after": "always-single-line",
-  //   "declaration-colon-space-before": "never",
-  //   "declaration-block-trailing-semicolon": "always",
-  //   "rule-empty-line-before": [
-  //     "always",
-  //     {
-  //       ignore: ["after-comment", "first-nested"]
-  //     }
-  //   ]
-  // }
-}
+  root: true,
+  extends: ['stylelint-config-standard-scss', "stylelint-config-recess-order", 'stylelint-config-prettier'],
+  plugins: ['stylelint-order'],
+  rules: {
+    "declaration-colon-space-after": "always-single-line",
+    "declaration-colon-space-before": "never",
+    "declaration-block-trailing-semicolon": "always",
+    "rule-empty-line-before": [
+      "always",
+      {
+        ignore: ["after-comment", "first-nested"]
+      }
+    ]
+  }
+};
