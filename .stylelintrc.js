f815e689c8490b7746f95cd294be79447d114579
wenlin <569958268@qq.com>
2021-06-17 20:44:43 +0800

    feat: stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,16 +1,5 @@
 module.exports = {
   root: true,
-  extends: ['stylelint-config-standard-scss', "stylelint-config-recess-order", 'stylelint-config-prettier'],
-  plugins: ['stylelint-order'],
-  rules: {
-    "declaration-colon-space-after": "always-single-line",
-    "declaration-colon-space-before": "never",
-    "declaration-block-trailing-semicolon": "always",
-    "rule-empty-line-before": [
-      "always",
-      {
-        ignore: ["after-comment", "first-nested"]
-      }
-    ]
-  }
+  extends: ['stylelint-config-standard', "stylelint-config-recess-order"],
+  plugins: ['stylelint-order'] ,
 };
---
+++ README.md
@@ -0,0 +1,2 @@
+ vue2 ts的eslint/stylelint校验规则
+ vue cli的ts prettier 加部分自定义。使用腾讯的alloy的eslint规则+prettier。stylelint增加规则，使用standard和recess-order排序
