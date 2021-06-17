359ead0a3a42cb437289706cd2eb93afd3edf2c0
wenlin <569958268@qq.com>
2021-06-17 22:20:31 +0800

    fix: 分号结尾

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
       'error',
       {
         endOfLine: 'auto', // 换行格式不做检查，换行格式有lf、cr，由于历史原因，windows下和linux下的文本文件的换行符不一致。
+        semi: false, // 末尾的分号
         printWidth: 120, // 单行长度，默认80
         singleQuote: true, // 单引号
         jsxSingleQuote: false, // jsx 不使用单引号，而使用双引号
@@ -19,4 +20,4 @@ module.exports = {
       }
     ]
   }
-};
+}
